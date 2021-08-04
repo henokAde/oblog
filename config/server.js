81d4e4c090a60b9ed428dffb5605386b4fa57969
henokADE <adeyamah@yahoo.fr>
2021-08-04 14:22:33 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '19138a17c09df703f5ea653dbc3fb3c6'),
+    },
+  },
+});
